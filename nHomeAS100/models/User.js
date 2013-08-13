64378f8707f11b0814a26cb0a6b18a39856e60a8
Lakshman Kumar Peethani <lakshman@nzentech.com>
2013-08-13 23:34:09 +0530

    DbManager changes

--- nHomeAS100/models/User.js
+++ nHomeAS100/models/User.js
@@ -1,8 +1,7 @@
 ﻿// The model definition is done in /path/to/models/project.js
 // As you might notice, the DataTypes are the very same as explained above
-var bcrypt = require('bcrypt-nodejs');
-
-var SALT_WORK_FACTOR = 10;
+//var bcrypt = require('bcrypt-nodejs');
+//var SALT_WORK_FACTOR = 10;
 
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define('User', {
@@ -12,16 +11,17 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING, get: function () {
                 return this.getDataValue('password');
             }, set: function (value) {
-                console.log("Value :" + value + "   " + bcrypt.getRounds(value));
-                if (isNaN(bcrypt.getRounds(value))) {
-                    console.log("Hashing password");
-                    var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
-                    var hash = bcrypt.hashSync(value, salt);
-                    console.log('In setter:' + value + ' hash: ' + hash);
-                    return this.setDataValue('password', hash);
-                } else {
-                    return this.setDataValue('password', value);
-                }
+                //console.log("Value :" + value + "   " + bcrypt.getRounds(value));
+                //if (isNaN(bcrypt.getRounds(value))) {
+                //    console.log("Hashing password");
+                //    var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+                //    var hash = bcrypt.hashSync(value, salt);
+                //    console.log('In setter:' + value + ' hash: ' + hash);
+                //    return this.setDataValue('password', hash);
+                //} else {
+                //    return this.setDataValue('password', value);
+                //}
+                return this.setDataValue('password', value);
             }
         },
         firstName: DataTypes.STRING,
@@ -30,11 +30,12 @@ module.exports = function (sequelize, DataTypes) {
     }, {
         instanceMethods: {
             comparePassword: function (candidatePassword) {
-                console.log('Comparing Password : ' + candidatePassword);
-                console.log('with hash: ' + this.password);
-                var result = bcrypt.compareSync(candidatePassword, this.password)
-                console.log('Password comparision result :' + result);
-                return result;
+                //console.log('Comparing Password : ' + candidatePassword);
+                //console.log('with hash: ' + this.password);
+                //var result = bcrypt.compareSync(candidatePassword, this.password)
+                //console.log('Password comparision result :' + result);
+                //return result;
+                return candidatePassword == this.password;
             },
             generateRandomToken : function () {
                 var user = this,
