bc5e6c93e710848a4e393ae9d1f9356b55246a33
Lakshman Kumar Peethani <lakshman@nzentech.com>
2013-08-13 11:19:41 +0530

    sequlize

--- nHomeAS100/sqlite_test.ts
+++ nHomeAS100/sqlite_test.ts
@@ -2,7 +2,7 @@
 
 var sqlite3 = require('sqlite3').verbose();
 
-var db = new sqlite3.Database('db/nhomeasdb.db');
+var db = new sqlite3.Database(':memory:');
 
 db.serialize(function () {
     db.run("CREATE TABLE lorem (info TEXT)");
