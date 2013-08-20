443ccb8912bd2713b9358b202604ce1256dc5df6
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2013-08-20 15:30:11 +0530

    Fixed the issue with device route

--- nHomeAS100/routes/devices.js
+++ nHomeAS100/routes/devices.js
@@ -1,8 +1,10 @@
 var devices = (function () {
     function devices(req, res) {
         if (req.params.id) {
+            console.log('Device Id:' + req.params.id);
             res.render('index', { title: 'Express', user: req.params.id });
         } else {
+            console.log('No device id specified');
             res.render('index', { title: 'Express', user: req.user });
         }
     }
--- nHomeAS100/routes/devices.ts
+++ nHomeAS100/routes/devices.ts
@@ -2,8 +2,10 @@
 export class devices {
     constructor(req, res) {
         if (req.params.id) {
+            console.log('Device Id:' + req.params.id);
             res.render('index', { title: 'Express', user: req.params.id });
         } else {
+            console.log('No device id specified');
             res.render('index', { title: 'Express', user: req.user });
         }
     }
