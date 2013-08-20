617c6e4e58f4329b6562674d1f206f07183a8212
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2013-08-20 16:26:08 +0530

    Fixed the issue with device route

--- nHomeAS100/routes/devices.js
+++ nHomeAS100/routes/devices.js
@@ -5,11 +5,11 @@ var devices = (function () {
         if (req.params.id) {
             console.log('Device Id:' + req.params.id);
             db.DbManager.Device.find({ where: { DeviceId: req.params.id } }).then(function (device) {
-                res.render('devices', { title: 'Device', user: req.params.id, device: device });
+                res.render('devices', { title: 'Device', user: req.params.id, device: device, devices: null });
             });
         } else {
             db.DbManager.Device.findAll().then(function (devices) {
-                res.render('devices', { title: 'Device List', user: req.user, devices: devices });
+                res.render('devices', { title: 'Device List', user: req.user, device: null, devices: devices });
             });
         }
     }
