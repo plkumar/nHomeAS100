49a7d67848d74a545ae2892cc33b069bbe72345e
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2013-08-20 16:08:54 +0530

    Fixed the issue with device route

--- nHomeAS100/routes/devices.js
+++ nHomeAS100/routes/devices.js
@@ -4,7 +4,9 @@ var devices = (function () {
     function devices(req, res) {
         if (req.params.id) {
             console.log('Device Id:' + req.params.id);
-            res.render('index', { title: 'Express', user: req.params.id });
+            db.DbManager.Device.find({ where: { DeviceId: req.params.id } }).then(function (device) {
+                res.render('index', { title: 'Device', user: req.params.id, device: device });
+            });
         } else {
             db.DbManager.Device.findAll().then(function (devices) {
                 res.render('devices', { title: 'Device List', user: req.user, devices: devices });
--- nHomeAS100/routes/devices.ts
+++ nHomeAS100/routes/devices.ts
@@ -4,7 +4,9 @@ export class devices {
     constructor(req, res) {
         if (req.params.id) {
             console.log('Device Id:' + req.params.id);
-            res.render('index', { title: 'Express', user: req.params.id });
+            db.DbManager.Device.find({ where: { DeviceId: req.params.id } }).then(function (device) {
+                res.render('index', { title: 'Device', user: req.params.id, device: device });
+            });
         } else {
             //console.log('No device id specified');
             db.DbManager.Device.findAll().then(function (devices) {
