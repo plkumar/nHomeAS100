4980d47c53b2d1a20815835dbebb69d65931b3ea
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2013-08-20 14:47:26 +0530

    added the devices route

---
+++ nHomeAS100/routes/devices.js
@@ -0,0 +1,12 @@
+var devices = (function () {
+    function devices(req, res) {
+        res.render('inedx', { title: 'Express', user: req.user });
+    }
+    devices.prototype.findById = function (req, res) {
+        res.render('index', { title: 'Express', user: req.params.id });
+    };
+    return devices;
+})();
+exports.devices = devices;
+;
+
---
+++ nHomeAS100/routes/devices.ts
@@ -0,0 +1,10 @@
+// Interface
+export class devices {
+    constructor(req, res) {
+        res.render('inedx', { title: 'Express', user: req.user });
+    }
+
+    public findById(req, res){
+        res.render('index', { title: 'Express', user: req.params.id });
+    }
+};
